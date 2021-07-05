3c4731e841bd264f2d9efa77fa1664fe8107d878
Yee <jmh_1024@163.com>
2021-07-05 13:49:37 +0800

    push_test_commit2

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 描述
+ *
+ * @author yee
+ * @version 1.0
+ */
 @SpringBootApplication
 public class Demo1Application {
 
